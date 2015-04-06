ef9ddaf9137c361320f8a5d9309e45729a1452fc
willedanielsson <william.l.danielsson@gmail.com>
2015-04-06 13:13:58 +0200

    Initial commit with working map using Mapbox API (based on leaflet)

---
+++ mapScript.js
@@ -0,0 +1,10 @@
+// Map id willedanielsson.llglgdhb
+
+//
+
+var initMap = function () {
+	L.mapbox.accessToken = 'pk.eyJ1Ijoid2lsbGVkYW5pZWxzc29uIiwiYSI6Ikh0a0tHZUkifQ.hdTiuBIY9KhNwr2vgA_pZw';
+
+	var map = L.mapbox.map('map', 'willedanielsson.llglgdhb').setView([58.397, 15.571], 12);
+}
+	
